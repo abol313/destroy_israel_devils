9250624924425a305e1191b3b53fc1ced3771c26
abol313 <FazlAbol18@gmail.com>
2022-04-28 23:03:14 +0430

    init: files base create but not ended...

---
+++ script.js
@@ -0,0 +1,13 @@
+const logger = document.querySelector("logger")
+const body = document.body
+const sequenses = ['Hit the israel...'
+,'...'
+,'devils in israel will soon be destroyed...'
+,'...'
+,'Thanks ALLAH'
+,'thank devil killers...'
+,'thank who all support...'
+,'...'
+]
+
+
